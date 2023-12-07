2767cb88f7f342c66b1f60f41d558006c6257e60
joycuicui <cuiqijoy@outlook.com>
2023-12-06 19:26:00 -0800

    modify renderTweets (empty, prepend), now new tweet shows without refreshing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,75 +5,53 @@
  */
 // Fake data taken from initial-tweets.json
 
+const createTweetElement = function (data) {
+  const $tweet = $("<article>");
+
+  // header
+  const $header = $("<header>");
+  const $userName = $("<span>").text(data.user.name);
+  const $avatars = $("<img>").attr("src", data.user.avatars);
+  const $handle = $("<span>").text(data.user.handle);
+  $header.append($avatars, $userName, $handle);
+
+  // content
+  const $content = $("<div>");
+  const $text = $("<p>").text(data.content.text);
+  $content.append($text);
+
+  //footer
+  const $footer = $("<footer>");
+  // const $timestamp = $("<span>").text(data.created_at);
+  const time = timeago.format(data.created_at);
+  const $timestamp = $("<span>").text(time);
+  const $icons = $("<div>").addClass("icons");
+  const $flag = $("<i>").addClass("fa-solid fa-flag");
+  const $retweet = $("<i>").addClass("fa-solid fa-retweet");
+  const $heart = $("<i>").addClass("fa-solid fa-heart");
+  $icons.append($flag, $retweet, $heart);
+  $footer.append($timestamp, $icons);
+
+  $tweet.append($header, $content, $footer);
+  return $tweet;
+};
+
+const renderTweets = function (tweetArray) {
+  $(".tweet-container").empty();
+  tweetArray.forEach((tweetData) => {
+    const $tweet = createTweetElement(tweetData);
+    $(".tweet-container").prepend($tweet);
+  });
+};
+
+const loadTweets = function () {
+  $.ajax("/tweets", { method: "GET" }).then(function (data) {
+    renderTweets(data);
+  });
+};
+
 $(document).ready(function () {
-  // const data = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac",
-  //     },
-  //     content: {
-  //       text: "If I have seen further it is by standing on the shoulders of giants",
-  //     },
-  //     created_at: 1461116232227,
-  //   },
-  //   {
-  //     user: {
-  //       name: "Descartes",
-  //       avatars: "https://i.imgur.com/nlhLi3I.png",
-  //       handle: "@rd",
-  //     },
-  //     content: {
-  //       text: "Je pense , donc je suis",
-  //     },
-  //     created_at: 1461113959088,
-  //   },
-  // ];
-
-  const createTweetElement = function (data) {
-    const $tweet = $("<article>");
-
-    // header
-    const $header = $("<header>");
-    const $userName = $("<span>").text(data.user.name);
-    const $avatars = $("<img>").attr("src", data.user.avatars);
-    const $handle = $("<span>").text(data.user.handle);
-    $header.append($avatars, $userName, $handle);
-
-    // content
-    const $content = $("<div>");
-    const $text = $("<p>").text(data.content.text);
-    $content.append($text);
-
-    //footer
-    const $footer = $("<footer>");
-    // const $timestamp = $("<span>").text(data.created_at);
-    const time = timeago.format(data.created_at);
-    const $timestamp = $("<span>").text(time);
-    const $icons = $("<div>").addClass("icons");
-    const $flag = $("<i>").addClass("fa-solid fa-flag");
-    const $retweet = $("<i>").addClass("fa-solid fa-retweet");
-    const $heart = $("<i>").addClass("fa-solid fa-heart");
-    $icons.append($flag, $retweet, $heart);
-    $footer.append($timestamp, $icons);
-
-    $tweet.append($header, $content, $footer);
-    return $tweet;
-  };
-
-  // const $test = createTweetElement(tweetData);
-  // console.log($test);
-  // $(".tweet-container").append($test);
-
-  const renderTweets = function (tweetArray) {
-    tweetArray.forEach((tweetData) => {
-      const $tweet = createTweetElement(tweetData);
-      $(".tweet-container").append($tweet);
-    });
-  };
-
-  // renderTweets(data);
+  loadTweets();
 
   // event listener for submit
   $("#tweet-form").on("submit", function (event) {
@@ -84,36 +62,22 @@ $(document).ready(function () {
     // submit a POST request that sends the serialized data to the server
 
     const tweetText = $("#tweet-text").val();
+
     if (!tweetText) {
       alert("Empty tweet!");
-    } else if (tweetText.length > 140) {
-      alert("Tweet too long!");
-    } else {
-      $.ajax({
-        method: "POST",
-        data: data,
-        url: "/tweets",
-      });
+      return;
     }
-  });
+    if (tweetText.length > 140) {
+      alert("Tweet too long!");
+      return;
+    }
 
-  // $(function () {
-  //   const $button = $("#load-more-posts");
-  //   $button.on("click", function () {
-  //     console.log("Button clicked, performing ajax call...");
-  //     $.ajax("more-posts.html", { method: "GET" }).then(function (
-  //       morePostsHtml
-  //     ) {
-  //       console.log("Success: ", morePostsHtml);
-  //       $button.replaceWith(morePostsHtml);
-  //     });
-  //   });
-  // });
-
-  const loadTweets = function () {
-    $.ajax("/tweets", { method: "GET" }).then(function (data) {
-      renderTweets(data);
+    $.ajax({
+      method: "POST",
+      data: data,
+      url: "/tweets",
+    }).then((response) => {
+      loadTweets();
     });
-  };
-  loadTweets();
+  });
 });
